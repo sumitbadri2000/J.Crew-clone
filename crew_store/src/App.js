da3614b9a8c35f05c541c053908fe8d4399240c4
sumitbadri2000 <sumitbadri2000@gmail.com>
2022-12-14 23:24:27 +0530

    day2

--- crew_store/src/App.js
+++ crew_store/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
